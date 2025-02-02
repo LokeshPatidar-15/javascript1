817b2c2872bc7f4b99b47844437131cd5f6a5eab
Lokesh Patidar <patidarlokesh1254@gmail.com>
2025-02-02 22:07:02 +0530

    added

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountid = 434564; 
+let mail = "zoro@gmail.com";
+var accountpassword = "12345";
+accountcity = "Bhopal ";
+
+console.log(accountpassword);
+accountpassword = "11111";
+console.log(accountpassword);
+
+console.table([mail,accountpassword,accountcity]);
--- test.js
+++ test.js
@@ -1,2 +1,1 @@
 console.log("Lokesh")
-console.log("patidar")
