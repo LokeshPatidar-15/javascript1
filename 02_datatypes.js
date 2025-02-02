5b5e5b0f4e4dfd78ca55d93909a3f879568f287c
Lokesh Patidar <patidarlokesh1254@gmail.com>
2025-02-02 22:19:58 +0530

    added

---
+++ 02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all the js code as newer version
+
+let name = "Lokesh"
+let age = 18
+let islogged = true
+
+console.log(typeof undefined); // undefine
+console.log(typeof null); //object
