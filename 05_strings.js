3d3a395a36a8c91a607e0f38998fe44505ce0ab5
Lokesh Patidar <patidarlokesh1254@gmail.com>
2025-02-03 22:09:18 +0530

    add

---
+++ 05_strings.js
@@ -0,0 +1,17 @@
+let name = 'zoro'
+let sword = 3
+console.log(`my name is  ${name} and i use ${sword} swords  `);
+
+let n = "substring";
+let m = n.substring(0,4)
+console.log(m);
+
+let n1= "substring";
+let m1 = n1.slice(-7,4);
+console.log(m1); 
+
+const stringtrim = "     lokesh.   "
+console.log(stringtrim.trim());
+console.log(stringtrim);
+
+
