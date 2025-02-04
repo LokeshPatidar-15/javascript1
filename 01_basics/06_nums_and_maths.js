31e77e1a55b5e25299a49c4576943ddc9f6bb301
Lokesh Patidar <patidarlokesh1254@gmail.com>
2025-02-04 12:10:18 +0530

    added

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,7 @@
+const balance =new Number(100)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+// *************************
+//  maths
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,6 @@
+ let date = new Date();
+ console.log(date);
+ console.log(date.toDateString());
+ console.log(date.toISOString());
+ console.log(date.toJSON());
+ console.log(date.toLocaleDateString());
