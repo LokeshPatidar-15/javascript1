68f03dc643d67392c67f62b1df0ea114a8c9f462
Lokesh Patidar <patidarlokesh1254@gmail.com>
2025-02-05 11:54:39 +0530

    add

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,25 @@
+ 
+if (true) {
+    let a =10
+    const b =20
+    var c = 30
+}
+
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+ function one (){
+    const captan = "Luffy";
+
+    function two(){
+        const swordsman = "Zoro"
+        console.log(captan);
+        
+    }
+    two()
+    // console.log(swordsman);
+ }
+ one()
+
+ 
