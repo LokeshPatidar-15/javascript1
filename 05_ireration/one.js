c42ca0ffd2f4c371216fae9af67277adc79d0620
Lokesh Patidar <patidarlokesh1254@gmail.com>
2025-02-07 16:37:19 +0530

    add

---
+++ 05_ireration/one.js
@@ -0,0 +1,17 @@
+const coding = [ "c++" , "JavaScript" , "python" , "ruby" , "swift"]
+
+// const values = coding.forEach((item) =>{
+//     console.log(item);
+//     return item
+    
+// })
+
+// console.log(typeof(values));
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+const newNums = myNums.filter((num) => num>4)
+console.log(newNums);
+
+
+
+
